4cf9d432a2656dff34e2316ae3faecafe5d1a3be
Zeroth <172555825+righCirith@users.noreply.github.com>
2024-10-13 21:46:45 -0600

    Create initials.cpp

---
+++ Module 1: Hellow World/initials.cpp
@@ -0,0 +1,27 @@
+/*
+Created on Sunday Oct 13 21:37:35 2024
+Author: @righCirith
+Module: 1: Hello World
+Project: Block Letters
+*/
+
+// Instructions: Write a C++ program called initials.cpp that displays the initials of your name in block letters as shown. (image is inside the folder).
+
+
+#include <iostream>
+int main() {
+  	// Print "R"
+    std::cout << "RRRR  \n";
+    std::cout << "R   R \n";
+    std::cout << "RRRR  \n";
+    std::cout << "R R   \n";
+    std::cout << "R  RR \n";
+
+    // Print "C"
+    std::cout << " CCCC \n";
+    std::cout << "C     \n";
+    std::cout << "C     \n";
+    std::cout << " CCCC \n";
+
+    return 0;
+}
