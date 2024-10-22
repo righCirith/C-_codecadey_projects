156c948996ff1baa0311a44e76e69c38a993d017
Zeroth <172555825+righCirith@users.noreply.github.com>
2024-10-21 18:12:09 -0600

    Create introduction.cpp

---
+++ 7_Functions/Challenges/introduction.cpp
@@ -0,0 +1,25 @@
+/*
+Created on Monday Oct 21 18:09:10 2024
+Author: @righCirith
+Module 6: Vectors
+Challenge 1: Introduction
+*/
+
+/*Instructions:
+Write a function introduction() with no return value that has:
+    - std::string parameter named first_name.
+    - std::string parameter named last_name.
+The function should print the last_name, followed by a comma, a space, first_name another space, and finally last_name again.
+For example, introduction("James", "Bond"); should print the following: Bond, James Bond
+*/
+
+#include <iostream>
+
+void introduction(std::string first_name, std::string last_name) {
+    std::cout << last_name << ", " << first_name << " " << last_name << "\n";
+}
+
+int main(){
+    introduction("James", "Bond");
+    return 0;
+}
