454cd9cd46bfc1dba28dd844810fde8e4f6e224a
Zeroth <172555825+righCirith@users.noreply.github.com>
2024-10-23 23:27:27 -0600

    Create ufo.cpp

---
+++ 7_Functions/Project_UFO/ufo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+    greet();
+
+    std::string codeword = "codecademy";
+    std::string answer = "__________";
+    int misses = 0;
+
+    std::vector<char> incorrect = {};
+    bool guess = false;
+    char letter;
+
+
+    while(answer != codeword && misses < 7) {
+        display_misses(misses);
+        display_status(incorrect, answer);
+
+        std::cout << "\nPlease enter your guess: ";
+        std::cin >> letter;
+
+        for(int k = 0;k < codeword.size(); k++) {
+            if(letter == codeword[k]) {
+                answer[k] = letter;
+                guess = true;
+            } 
+        }
+
+        if(guess == true) {
+            std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+        } else {
+            std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+            incorrect.push_back(letter);
+            misses++;
+        }
+
+         guess = false;
+    }
+
+    end_game(answer, codeword);
+
+    return 0;
+}
